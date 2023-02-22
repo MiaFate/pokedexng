87bb917c328a197c783f6b28c23011212220a8e9
Mia Ramos <52300099+MiaFate@users.noreply.github.com>
2023-02-22 01:47:21 -0300

    Update main.ts

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 import * as cors from 'cors';
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.use(cors());
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
